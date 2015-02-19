847f5d1720b64ef0bf4911d3be791a694ba0ba70
Przemek <sienkowskip@gmail.com>
2015-02-19 09:45:55 +0100

    added validation and #average_rating in Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+
+  def average_rating
+    reviews.average(:rating)
+  end
+
 end
