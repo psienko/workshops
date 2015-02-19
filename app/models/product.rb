537a75d20b0fa598549ad8d76243c39b84bc055d
Przemek <sienkowskip@gmail.com>
2015-02-19 10:12:21 +0100

    added format validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,9 @@ class Product < ActiveRecord::Base
   has_many :reviews
 
   validates_presence_of :title, :description, :price
+  validates_format_of :price, with: /\A\d+\.\d{1,2}\z/
 
   def average_rating
     reviews.average(:rating)
   end
-
 end
