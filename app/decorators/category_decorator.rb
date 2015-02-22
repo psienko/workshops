6d6dd82b927b956df19a47385493758b9ab2717d
Przemek <sienkowskip@gmail.com>
2015-02-22 11:20:37 +0100

    modified displaying 'New Category' link only for admin

--- app/decorators/category_decorator.rb
+++ app/decorators/category_decorator.rb
@@ -1,13 +1,9 @@
 class CategoryDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def new_category
+    if h.current_user && h.current_user.admin?
+      h.link_to 'New Category', h.new_category_path
+    end
+  end
 end
--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -2,13 +2,30 @@ require 'spec_helper'
 
 describe CategoryDecorator do
 
+  let(:admin) { build(:user, admin: true) }
   let(:user) { build(:user) }
   let(:category) { described_class.new(build(:category)) }
 
   context 'user is an admin' do
+    before do
+      sign_in admin
+    end
+
     describe '#new_category' do
       it "displays the 'New Category' link" do
         expect(category.new_category).to have_selector('a', text: 'New Category')
+      end
+    end
+  end
+
+  context 'user is not an admin' do
+    before do
+      sign_in user
+    end
+
+    describe '#new_category' do
+      it "does not display the 'New Category' link" do
+        expect(category.new_category).to be_nil
       end
     end
   end
