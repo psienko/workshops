6a66ad8cf91414aef476f02a555710ec7545114a
Przemek <sienkowskip@gmail.com>
2015-02-22 10:02:45 +0100

    added spec for CategoryDecorator

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe CategoryDecorator do
+
+  let(:user) { build(:user) }
+  let(:category) { described_class.new(build(:category)) }
+
+  context 'user is an admin' do
+    describe '#new_category' do
+      it "displays the 'New Category' link" do
+        expect(category.new_category).to have_selector('a', text: 'New Category')
+      end
+    end
+  end
 end
