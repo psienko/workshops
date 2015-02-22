26d43adcc2b6a4cff2b21878d1b99ad661db8910
Przemek <sienkowskip@gmail.com>
2015-02-22 12:40:57 +0100

    added spec for ProductDecorator

--- spec/decorators/product_decorator_spec.rb
+++ spec/decorators/product_decorator_spec.rb
@@ -1,4 +1,34 @@
 require 'spec_helper'
 
 describe ProductDecorator do
+
+  let(:user) { build(:user) }
+  let(:owner) { build(:user) }
+  let(:product) { described_class.new(build(:product, user: owner)) }
+  
+  context 'user is the owner of product' do
+
+    before do
+      sign_in owner
+    end
+    
+    describe '#edit_product' do
+      it "displays the 'Edit' link" do
+        expect(product.edit_product).to have_selector('a', text: 'Edit')
+      end
+    end
+  end
+
+  context 'user is not the owner of product' do
+
+    before do
+      sign_in user
+    end
+    
+    describe '#edit_product' do
+      it "does not display the 'Edit' link" do
+        expect(product.edit_product).to be_nil
+      end
+    end
+  end
 end
