c198ddff059e3c3fb9925c3a918b27744d37fc59
Przemek <sienkowskip@gmail.com>
2015-02-24 19:17:07 +0100

    added new methods

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -5,4 +5,17 @@ class UserDecorator < Draper::Decorator
     return 'admin' if h.current_user.admin?
     'standard user'
   end
+
+  def display_edit_button_in_panel_footer
+    return nil if h.current_user != object
+    h.content_tag :div, class: 'panel-footer row' do
+      h.content_tag :div, class: 'pull-right' do
+        edit
+      end
+    end
+  end
+
+  def edit
+    h.link_to 'Edit My Profile', h.edit_user_registration_path, class: 'btn btn-lg btn-success'
+  end
 end
