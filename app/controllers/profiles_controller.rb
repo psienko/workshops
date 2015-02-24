e702925eda75c20e5aba180f2a9d8eef38b650aa
Przemek <sienkowskip@gmail.com>
2015-02-24 22:01:29 +0100

    fixed exposure

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,9 +1,8 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
-  expose(:reviews) { user.reviews.order('created_at desc').limit(5) }
-  expose(:user)
   expose_decorated(:user)
+  expose_decorated(:reviews) { user.reviews.order('created_at desc').limit(5) }
 
   def show
   end
