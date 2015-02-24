ef1a07711dd11e649c59a6a15effb61e2e2f319a
Przemek <sienkowskip@gmail.com>
2015-02-24 16:59:39 +0100

    fixed spec

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -13,26 +13,26 @@ describe ProfilesController do
 
     describe 'GET show' do
       it 'returns http success' do
-        get :show
+        get :show, { id: user.to_param }
         expect(response).to have_http_status(:success)
       end
 
       it 'renders the profile page template' do
-        get :show
+        get :show, { id: user.to_param }
         expect(response).to render_template('show')
       end
 
       it 'exposes the user products' do
         create :product, user_id: user.id
         create :product
-        get :show
+        get :show, { id: user.to_param }
         expect(controller.products).to eq(user.products)
       end
 
       it 'exposes the user reviews' do
         create :review, user_id: user.id
         create :review
-        get :show
+        get :show, { id: user.to_param }
         expect(controller.reviews).to eq(user.reviews)
       end
     end
@@ -41,7 +41,7 @@ describe ProfilesController do
   context 'user is not signed in' do
     describe 'GET show' do
       it 'redirects the user to the login page' do
-        get :show
+        get :show, { id: user.to_param }
         expect(response).to redirect_to(new_user_session_path)
       end
     end
