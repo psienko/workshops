ca4c3f822b00be8bb9e2ba1235407d9a009ff2c6
Przemek <sienkowskip@gmail.com>
2015-02-24 16:44:21 +0100

    modified profiles_controller.rb

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+
+  expose(:products) { current_user.products }
+  expose(:reviews) { current_user.reviews }
+
   def show
   end
 end
